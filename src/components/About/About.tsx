b7e860d06c2fd56c843990571ea0ee8543717ffe
Batoul7 <batoulab268@gmail.com>
2025-01-25 15:49:00 +0300

    update

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,5 @@
 import { faDownload} from '@fortawesome/free-solid-svg-icons'
-import me from '../../assets/me.jpg' 
+import me from '../../assets/me.webp' 
 import Title from '../Title/Title'
 import Box from '../Box/Box'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -20,7 +20,7 @@ const About = () => {
                 <div className='flex items-center justify-center sm:justify-start gap-2 sm:gap-3'>
                   <Box data={BoxData} style='w-full sm:w-[120px]' />
                 </div>
-                <p  data-aos="fade-up" >I am a passionate front-end developer specializing in creating engaging user experiences. I focus on transforming ideas into dynamic applications with an emphasis on responsive design and optimal performance. I am eager to take on new projects and deliver innovative solutions that meet user needs.
+                <p  data-aos="fade-up" >I’m a software engineer specializing in front-end development, with extensive experience in designing and building user interfaces using React. Strong skills in transforming creative ideas into interactive web applications enhance the user experience. Experience across various projects has fostered an understanding of customer needs and the ability to translate them into effective technical solutions. Committed to staying up to date with the latest trends and technologies, with a strong belief in the importance of collaboration and teamwork. A creative and dedicated front-end developer ready to add real value to any team.
                 </p>
                 <a  data-aos="fade-up"  href={cv} download="batoul_Alibraheem.pdf"
                   className="py-1 px-4 shadow-btnshadow rounded-lg text-base md:text-lg font-medium font-poppins cursor-pointer
